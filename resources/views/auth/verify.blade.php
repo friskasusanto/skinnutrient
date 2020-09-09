5a214856de2aae4572a7f133d190ea916e8b9dea
friska susanto <friskasusanto8@gmail.com>
2020-09-09 12:41:07 +0700

    email verivication

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,28 +1,56 @@
 @extends('frontend.layout.index')
-
 @section('content')
+
+
+<!-- breadcrumb start -->
+    <div class="breadcrumb-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="page-title">
+                        <h2>customer's login</h2>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <nav aria-label="breadcrumb" class="theme-breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                            <li class="breadcrumb-item active">login verify</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- breadcrumb End -->
+
+<br/>
+<br/>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">Verifikasi alamat email anda</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            Email verifikasi baru saja kami kirim ke email anda
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    Sebelum Login kembali, tolong cek verifikasi link konfirmasi yang sudah kami kirim ke email anda. <br/>
+                    Jika anda tidak menerima email tersebut, <br/>
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">Klik disini untuk kiriman email konfirmasi lain</button>.
                     </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<br/>
+<br/>
+
 @endsection
