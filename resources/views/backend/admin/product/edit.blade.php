64f2917494b710dfcee26a7f3a2f211890eff26a
friska susanto <friskasusanto8@gmail.com>
2020-11-19 15:11:50 +0700

    edit

--- resources/views/backend/admin/product/edit.blade.php
+++ resources/views/backend/admin/product/edit.blade.php
@@ -73,20 +73,6 @@
                                     <div class="valid-feedback">Looks good!</div>
                                 </div>
                                 <div class="form-group mb-3 row">
-                                    <label for="validationCustom01" class="col-xl-3 col-sm-4 mb-0">Jenis Product :</label>
-
-                                    <div class="col-xl-8 col-sm-7 pl-0 description-sm">
-                                        <select name="jenis" type="text" class="form-control" style="width: 100%">
-                                            <option value="">--{{$product->jenis->name}}--</option>
-                                            <option value="">--pilih jenis product--</option>
-                                            @foreach ($jenis as $p)
-                                                <option value= "{{$p->id}}">{{$p->jenis}}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    <div class="valid-feedback">Looks good!</div>
-                                </div>
-                                <div class="form-group mb-3 row">
                                     <label for="validationCustom02" class="col-xl-3 col-sm-4 mb-0">Harga User:</label>
                                     <input class="form-control col-xl-8 col-sm-7" id="validationCustom02" type="number" required="" name="price" value="{{$product->price}}">
                                     <div class="valid-feedback">Looks good!</div>
