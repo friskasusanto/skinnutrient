c005a906040f0716ab921b1fdaa88ac401ff131d
friska susanto <friskasusanto8@gmail.com>
2021-01-22 13:37:48 +0700

    url home

--- resources/views/frontend/detailProduct.blade.php
+++ resources/views/frontend/detailProduct.blade.php
@@ -14,7 +14,7 @@ s@extends('frontend.layout.index', ['active' => 'home'])
                 <div class="col-sm-6">
                     <nav aria-label="breadcrumb" class="theme-breadcrumb">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
                             <li class="breadcrumb-item active" aria-current="page"> detail product</li>
                         </ol>
                     </nav>
