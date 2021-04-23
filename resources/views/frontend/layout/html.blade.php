9b640b6d262f06e517dfe9892c744c6e38315627
friska susanto <friskasusanto8@gmail.com>
2021-04-23 15:06:29 +0700

    google pixel

--- resources/views/frontend/layout/html.blade.php
+++ resources/views/frontend/layout/html.blade.php
@@ -31,6 +31,24 @@
     <!-- Theme css -->
     <link rel="stylesheet" type="text/css" href="{{asset('backends/assets/css/color2.css')}}" media="screen" id="color">
 
+    <!-- Facebook Pixel Code -->
+        <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '447822012979039');
+            fbq('track', 'PageView');
+        </script>
+        <noscript>
+            <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=447822012979039&ev=PageView&noscript=1" />
+        </noscript>
+        <!-- End Facebook Pixel Code -->
+
     <!-- Website Google Analytics for Skin Nutrient
     ID Pelacakan
     UA-192258117-1
