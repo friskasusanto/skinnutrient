80f1f0d54958b1f9d53023e259dace6584e6ec7a
friska susanto <friskasusanto8@gmail.com>
2021-07-26 09:11:34 +0700

    wa

--- resources/views/frontend/layout/frontend/blog-detail.blade.php
+++ resources/views/frontend/layout/frontend/blog-detail.blade.php
@@ -29,7 +29,7 @@
                     <nav aria-label="breadcrumb" class="theme-breadcrumb">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
-                            <li class="breadcrumb-item"><a href="{{url('/blog')}}">Blog</a></li>
+                            <li class="breadcrumb-item"><a href="{{url('/blogs')}}">Blog</a></li>
                             <li class="breadcrumb-item active" aria-current="page">Blogs</li>
                         </ol>
                     </nav>
@@ -44,33 +44,14 @@
     <section class="blog-detail-page section-b-space ratio2_3">
         <div class="container">
             <div class="row">
-                <div class="col-sm-12 blog-detail"><img src="{{asset('frontend/assets/images/about/about%20us.jpg')}}"
+                <div class="col-sm-12 blog-detail"><img src="{{url('blog/'.$blog->images)}}"
                         class="img-fluid blur-up lazyload" alt="">
-                    <h3>Also the leap into electronic typesetting, remaining essentially unchanged.</h3>
+                    <h3>{{$blog->judul}}</h3>
                     <ul class="post-social">
-                        <li>25 Juli 2021</li>
-                        <li>Posted By : Admin Skin Nutrient Indonesia</li>
+                        <li>{{$blog->tgl_input}}</li>
+                        <li>Posted By : {{$blog->user->name}}</li>
                     </ul>
-                    <p>Fusce scelerisque augue a viverra semper. Etiam nisi nibh, vestibulum quis augue id, imperdiet
-                        fringilla dolor. Nulla sed nisl vel nisi cursus finibus. Vivamus ut augue nec justo viverra
-                        laoreet. Nunc efficitur, arcu ac cursus gravida, lorem elit commodo urna, id viverra libero
-                        tellus non ipsum. Fusce molestie ultrices nibh feugiat pretium. Donec pulvinar arcu metus, et
-                        dapibus odio condimentum id. Quisque malesuada mauris sit amet dui feugiat, ut pretium mauris
-                        luctus. Ut aliquam, tellus nec molestie condimentum, tellus arcu dignissim quam, a gravida nunc
-                        nulla vel magna. Sed pulvinar tortor et euismod blandit. Proin accumsan orci ac nunc fermentum
-                        vehicula.</p>
-                    <p>Cras quis neque urna. Pellentesque mollis, dui nec elementum elementum, ipsum quam suscipit
-                        ligula, sit amet lobortis dolor sem sed neque. Vivamus consequat est non sodales efficitur.
-                        Aliquam sodales eleifend sodales. Aliquam auctor ipsum quis nisl facilisis, at convallis mauris
-                        iaculis. Duis eleifend, magna ac convallis blandit, dui dui auctor leo, sed tincidunt nisi
-                        mauris ut nulla. Praesent porttitor dui ac turpis commodo porttitor. Integer ligula nisi,
-                        bibendum non sem at, porta condimentum dui.</p>
-                    <p>Proin id eleifend diam, euismod dictum nibh. Ut ullamcorper in purus at tempor. Nullam mattis
-                        risus nec velit semper lobortis. Donec accumsan ligula fermentum, ultricies massa eget, cursus
-                        leo. Suspendisse placerat elit et lacus aliquam, ut elementum leo aliquet. Integer ornare, ipsum
-                        eu lacinia viverra, lectus ipsum scelerisque nisl, nec iaculis leo elit id arcu. Aliquam id ante
-                        elit. Donec commodo purus eget lacus pharetra, et egestas metus blandit. Quisque pellentesque
-                        porta urna, sed dictum enim viverra a.</p>
+                    <p>{!!$blog->text!!}</p>
                 </div>
             </div>
             <div class="row section-b-space blog-advance">
